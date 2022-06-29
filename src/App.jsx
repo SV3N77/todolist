796abe09e7607163d6996eabfd2441ab26e28b1b
SV3N77 <50192528+SV3N77@users.noreply.github.com>
2022-06-29 11:22:31 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -146,7 +146,7 @@ function TodoItems({ todo, onDeleteTask, onEditTask }) {
     </form>
   ) : (
     <div className="flex flex-col gap-3 w-full">
-      <div className="text-lg bg-slate-300 px-1 rounded-lg">{todo.title}</div>
+      <div className="text-lg bg-slate-300 px-2 rounded-lg">{todo.title}</div>
       <div className="text-sm px-1">{todo.task}</div>
       <div className="flex flex-row gap-2 ml-auto absolute bottom-1 right-2">
         <Button onClick={() => setIsEditing(true)}>Edit</Button>
